ba8060c02bd265ed9181462e368e78ba40617e59
savith nishitha <isavith.nishitha@gmail.com>
2021-05-17 17:08:54 +0530

    first commit

--- git.sh
+++ git.sh
@@ -1,8 +1,8 @@
 git config --global user.name "savith nishitha"
 git config --global user.email "isavith.nishitha@gmail.com"
 git init
-git branch -M main
 git commit -m "first commit"
 git remote add https://github.com/savith12321/sky-line.git
-git add *
+git branch -M main
+git add .
 git push -u origin master
